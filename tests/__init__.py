6c11d98c8136ee400a15b0ccda1f472729374026
kyriatide <8896439+kyriatide@users.noreply.github.com>
2022-06-21 21:39:38 +0200

    Initial test branch commit.

---
+++ tests/__init__.py
@@ -0,0 +1,23 @@
+import os, pathlib, shutil
+
+DIR_TESTS = os.path.join(pathlib.Path(__file__).parent.absolute())
+
+DIR_TESTS_CONFIG_SOURCE = os.path.join(DIR_TESTS, 'config.source')
+DIR_TESTS_CONIFG = os.path.join(DIR_TESTS, 'config')
+
+
+def setup():
+    tear_down()
+    os.makedirs(DIR_TESTS_CONIFG, exist_ok=True)
+
+    # copy files
+    sync_files = {os.path.join(DIR_TESTS_CONFIG_SOURCE, 'example.conf'): os.path.join(DIR_TESTS_CONIFG, 'example.conf'),
+                  os.path.join(DIR_TESTS_CONFIG_SOURCE, 'example.conf.tmpl'): os.path.join(DIR_TESTS_CONIFG, 'example.conf.tmpl')}
+    for source, target in sync_files.items():
+        print('Copy {} to {} ...'.format(source, target))
+        shutil.copy(source, target)
+
+
+def tear_down():
+    if os.path.exists(DIR_TESTS_CONIFG):
+        shutil.rmtree(DIR_TESTS_CONIFG)
